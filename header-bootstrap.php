d1c0e247e58bd40cac950e5e9831811f046bef5d
AmitDeo <magpie.arise@gmail.com>
2015-08-14 11:29:29 +0545

    Sundance New Homepage

---
+++ header-bootstrap.php
@@ -0,0 +1,120 @@
+<?php
+/**
+ * The Header for our theme.
+ *
+ * Displays all of the <head> section and everything up till <div id="main">
+ *
+ * @package Sundance
+ * @subpackage Sundance
+ * @since Sundance 2.0
+ */
+
+?><!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+    <meta charset="<?php bloginfo( 'charset' ); ?>" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="profile" href="http://gmpg.org/xfn/11" />
+	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
+	<link href="https://plus.google.com/107104241400965217576/posts" rel="publisher" />
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>
+		<?php wp_title( '' ); ?>
+		<?php
+		if ( $_GET['bvrrp'] )
+			echo ' Reviews';
+		if ( $_GET['bvqap'] )
+			echo ' Questions';
+		?>
+	</title>
+
+    <!-- Bootstrap -->
+    <link href="<?php get_bloginfo('template_url'); ?>/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="<?php get_bloginfo('template_url'); ?>/bootstrap/css/custom.css" rel="stylesheet">
+    
+    <link href='http://fonts.googleapis.com/css?family=Lato:400,700,900|Roboto:400,700,900' rel='stylesheet' type='text/css'>
+	
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+    <?php wp_enqueue_script('jquery'); ?>
+    <script src="<?php get_bloginfo('template_url'); ?>/bootstrap/js/bootstrap.min.js"></script>
+  </head>
+  
+<body <?php body_class(); ?>>
+
+	<?php custom_data_layer(); ?>
+	
+	<?php google_tag_manager(); ?>
+
+	<?php google_tag_manager_criteo(); ?>
+
+    <section class="wrapper headercontainer">
+  		<div class="container">
+  			<div class="row">
+  				<div class="col-xs-12 col-sm-5 col-md-5">
+  					<h1 class="logo"><a href="#">logo</a></h1>
+  				</div>
+  				<div class="col-xs-12 col-sm-7 col-md-7">
+  					<ul class="toplist">
+  						<li class="listborder"><a href="#">Free Brochure</a></li>
+  						<li><a href="#">Nearest Dealer</a></li>
+  						<li><form><input type="text" class="zip btn-start" placeholder="zip/postal code" /></form></li>
+  					</ul>
+  				</div>
+  			</div>		
+			<div class="row">
+				<div class="col-xs-12">
+					<nav class="navbar navbar-default" role="navigation">
+						<div class="container-fluid">
+							<!-- Brand and toggle get grouped for better mobile display -->
+							<div class="navbar-header">
+								<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+									<span class="sr-only">Toggle navigation</span>
+									<span class="icon-bar"></span>
+									<span class="icon-bar"></span>
+									<span class="icon-bar"></span>
+								</button>
+							</div>
+							<!-- Collect the nav links, forms, and other content for toggling -->
+							<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+										<ul class="nav navbar-nav">
+											<li class="listborder">
+												<a href="#">SPA<span class="sr-only">(current)</span></a>
+											</li>
+											<li class="listborder">
+												<a href="#">ABOUT US</a>
+											</li>
+											<li class="listborder dropdown">
+												<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">ACCESSORIES</a>
+												<ul class="dropdown-menu">
+										            <li><a href="#">Action</a></li>
+										            <li><a href="#">Another action</a></li>
+										            <li><a href="#">Something else here</a></li>
+										            <li role="separator" class="divider"></li>
+										            <li><a href="#">Separated link</a></li>
+										            <li role="separator" class="divider"></li>
+										            <li><a href="#">One more separated link</a></li>
+										          </ul>
+											</li>
+											<li class="listborder">
+												<a href="#">OWNER</a>
+											</li>
+											<li class="listborder">
+												<a href="#">GET PRICING</a>
+											</li>
+											<li class="search">
+												<a href="#">SEARCH</a>
+											</li>	
+										</ul>
+							</div>
+						</div><!-- /.container-fluid -->
+					</nav>
+				</div>
+			</div>
+  		</div>
+  	</section>
---
+++ template-homepage.php
@@ -0,0 +1,214 @@
+<?php
+/**
+ * Home Page template file
+ *
+ * @package Sundance
+ * @subpackage Sundance
+ * @since Sundance 2.0
+ */
+
+get_header('bootstrap'); ?>
+<section class="wrapper bannercontainer">
+  		<div class="container bigcontainer">
+  			<div class="row">
+  				<div class="col-xs-12 col-sm-7 col-md-7">
+  					
+  				</div>
+  				<div class="col-xs-12 col-sm-5 col-md-5">
+  					<div class="bannercaption">
+  						<h1>ALIQUAM FINIBUS TRISTI ODIO SIT AMET ULTRICES</h1>
+  						<p>Morbi condimentum ex ipsum, ac tempus est pharetra et. Nullam consequat erat vel sem vestibulum fermentum. Praesent maximus bibendum malesuada.</p>
+  						<a href="#" class="hot btn-start">VIEW HOT TUBS</a>
+  					</div>
+  				</div>
+  			</div>
+  		</div>
+  	</section>
+  	<section class="wrapper minicontainer">
+  		<div class="container smallcontainer">
+  			<div class="row ">
+  				<div class="col-xs-12 col-sm-3 col-md-3">
+  					<div class="selling">
+  						<div class="bestselling">
+  				<h3>BEST SELLING</h3>
+  				</div>
+  				</div>
+  				</div>
+  				<div class="col-xs-12 col-sm-3 col-md-3">
+  					<div class="therapy">
+  						<div class="bestherapy">
+  				<h3>THERAPY</h3>
+  				</div>
+  				</div> 				
+  				</div>
+  				<div class="col-xs-12 col-sm-3 col-md-3">
+  				<div class="entertaining">
+  						<div class="mostentertaining">
+  				<h3>ENTERTAINING</h3>
+  				</div>
+  				</div> 				
+  				</div> 				
+  				<div class="col-xs-12 col-sm-3 col-md-3">
+  				<div class="search">
+  						<div class="mostsearch">
+  				<h3>SEARCH BY SIZE</h3>
+  				</div>
+  				</div> 	
+  				</div>
+  			</div>
+  		</div>
+  	</section>
+  	<section class="wrapper whitecontainer">
+  		<div class="container smallcontainer">
+  			<div class="row whitecontent">
+  				<div class="col-xs-12 col-sm-6 col-md-6 firstcontent">
+  					<div class="row">
+  						<div class="col-xs-12 col-sm-8 col-md-7 padl0 padr0">
+  							<h3>Find Your Nearest Dealer</h3>
+  							<ul class="dealerlist">
+	  							<li>Aliquam finibus tristique odio sit amet</li>
+	  							<li>Posuere semper nisi vitae pharetra</li>
+	  							<li>Maecenas ante est</li>
+  							</ul>
+  							<form>
+  								<input type="firstname" class="form-control" id="exampleInputEmail3" placeholder=" Enter your zip code">
+  								<button type="button" class="blue btn-start">LOCATE DEALER</button>
+  							</form>
+  							
+  						</div>
+  						<div class="col-xs-12 col-sm-4 col-md-5 padl0 padr0">
+  							<img class="ximg-responsive" src="<?php bloginfo('template_url'); ?>/bootstrap/images/image5.png">
+  						</div>
+  					</div>
+  				</div>
+  				<div class="col-xs-12 col-sm-6 col-md-6">
+  					<div class="row">
+  						<div class="col-xs-12 col-sm-8 col-md-7 padr0">
+  							<h3>Free Brochure</h3>
+  							<ul class="brochurelist">
+	  							<li>Aliquam finibus tristique odio sit amet</li>
+	  							<li>Posuere semper nisi vitae pharetra</li>
+	  							<li>Tempus in pretium eu</li>
+	  							<li>Maecenas ante est</li>
+	  						</ul>	
+	  						<a href="#" class="btn btn-primary btn-blue">GET BROCHURE</a>
+  						</div>
+  						<div class="col-xs-12 col-sm-4 col-md-5 padl0 padr0">
+  							<img class="ximg-responsive" src="<?php bloginfo('template_url'); ?>/bootstrap/images/small_logo.png">
+  						</div>
+  					</div>
+  				</div>
+  			</div>
+  		</div>
+  	</section>
+  	<section class="wrapper smallcontainer">
+  		<div class="container smallcontainer">
+  			<div class="row smallcontent">
+  				<div class="col-xs-12 col-sm-3 col-md-3">
+  					<img class="img-responsive" src="<?php bloginfo('template_url'); ?>/bootstrap/images/truck.png">
+  				</div>
+  				<div class="col-xs-12 col-sm-5 col-md-5">
+  					<h1>Truckload <span>Savings Event</span></h1>
+  					<p>Take Advantage of the season's best discounts!</p>
+  				</div>
+  				<div class="col-xs-12 col-sm-4 col-md-4 dreambtncont">
+  					<a href="#" class="btn btn-primary btn-blue btn-big">GET THE SPA OF YOUR DREAMS</a>
+  				</div>
+  			</div>
+  		</div>
+  	</section>
+  	<section class="wrapper groupcontainer">
+  		<div class="container bigcontainer">
+  			<div class="row">
+  				<div class="col-xs-12 col-sm-4 col-md-4 shawor">
+  					  			
+  				</div>
+  				<div class="col-xs-12 col-sm-4 col-md-4 iphone">
+  					</div>
+  				<div class="col-xs-12 col-sm-4 col-md-4 bed">
+	  				 
+  				</div>
+  			</div>
+  			<div class="row hydrotherapy">
+  				<div class="col-xs-12 col-sm-4 col-md-4">
+	  				<h1>Hydrotherapy</h1>
+	  				<ul class="hydrotherapy">
+		  				<li>Aliquam finibus tristique odio sit amet</li>
+		  				<li>Posuere semper nisi vitae pharetra</li>
+		  				<li>Maecenas ante est</li>
+	  				</ul>
+  				</div>
+  				<div class="col-xs-12 col-sm-4 col-md-4">
+  					<h1>SunSmart WiFi App</h1>
+  					<ul class="hydrotherapy">
+	  					<li>Aliquam finibus tristique odio sit amet</li>
+	  					<li>Posuere semper nisi vitae pharetra</li>
+	  					<li>Maecenas ante est</li>
+	  				</ul>	
+  				</div>
+  				<div class="col-xs-12 col-sm-4 col-md-4">
+  					<h1>Warranty</h1>
+  					<ul class="hydrotherapy">
+	  					<li>Aliquam finibus tristique odio sit amet</li>
+	  					<li>Posuere semper nisi vitae pharetra</li>
+	  					<li>Maecenas ante est</li>
+	  				</ul>	
+  				</div>
+  			</div>
+  		</section>
+  		<section class="wrapper tipscontainer">
+  			
+  	</section>
+  	<section class="wrapper paracontainer">
+  		<div class="container smallcontainer">
+  			<div class="row paracontent">
+  				<div class="col-xs-12 col-sm-6 col-md-6">
+  					<img class="img-responsive" src="<?php bloginfo('template_url'); ?>/bootstrap/images/group_bath.png">
+  				</div>
+  				<div class="col-xs-12 col-sm-6 col-md-6">
+  					<div class="row">
+  						<div class="col-xs-12"> 					
+  						<p><span>YOU CAN BELIEVE IN A SUNDANCE SPA.</span> Established in 1979, Sundance Spas has been recognized internationally with more awards and honors than any other spa company. our exclusive components, processes, and features include the famous line of potented Fluidix jets. We are committed to using environmentally sale equipment and practices. Read about our distinguished history of spa innovation in the Sundance Spas brochure.</p>
+  					</div>
+  					</div> 					
+  					<div class="row">
+  						<div class="col-xs-12">
+  						<img class="img-responsive partnerlogo" src="<?php bloginfo('template_url'); ?>/bootstrap/images/partnerlogo.jpg">
+  						</div>
+  					</div>
+  				</div>
+  			</div>
+  			<div class="row news">
+  				<div class="col-xs-12 col-sm-4 col-md-4">
+  					<h1>Blog</h1>
+  					<p>From news on the best ways to care for your spa to how to get a better night's sleep. Learn and stay informed with our Sundance Spas Blog. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras a viverra odio. Curabitur...<a href="#">Read More</a></p>
+  				</div>
+  				<div class="col-xs-12 col-sm-4 col-md-4">
+  					<h1>Online Financing</h1>
+  					<p><span>Qualify for online financing now.</span> *Not valid with other not promotional offers. Restrictions apply. See participating dealers for complete terms and conditions. Financing available on approved credit.</p>
+  				</div>
+  				<div class="col-xs-12 col-sm-2 col-md-2 tub">
+  					<h1>Categories</h1>
+  					<ul class="categories">
+	  					<li><a href="#">Hot Tub Accessories</a></li>
+	  					<li><a href="#">Hot Tub Benefits</a></li>
+	  					<li><a href="#">Hot Tub Features</a></li>
+	  					<li><a href="#">Hot Tub Installation</a></li>
+	  					<li><a href="#">Hot Tub Maintenance</a></li>
+	  					<li><a href="#">Hot Tub News</a></li>
+	  				</ul>	
+  				</div>
+  				<div class="col-xs-12 col-sm-2 col-md-2 tub1">
+  					<ul class="rightcategories">
+	  					<li><a href="#">Hot Tub Reviews</li>
+	  					<li><a href="#">Hot Tub Sales and Promotions</a></li>
+	  					<li><a href="#">Hot Tub Series and Models</a></li>
+	  					<li><a href="#">Sundance Spas Dealers</a></li>
+	  					<li><a href="#">Sundance Spas Events</a></li>
+	  					<li><a href="#">Sundance Spas Lifestyle</a></li>
+	  				</ul>	
+  				</div>
+  			</div>
+  		</div>
+  	</section>
+<?php get_footer('bootstrap'); ?>
