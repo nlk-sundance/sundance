d38ad5a569f0803f5a14113621b0381eee89ed4c
Tim Spinks <tim@ninthlink.com>
2015-08-31 10:31:10 -0700

    JAC-909 title css updates

--- single-s_spa.php
+++ single-s_spa.php
@@ -138,7 +138,7 @@ dataLayer.push({
     'event':'pageReady'
 });
 </script>
-<div class="cols istub <?php if ( $ser->post_title == 'Select' ) { echo 'select'; } ?>">
+<div class="cols istub <?php echo strtolower( $ser->post_title ); ?>">
   <div itemscope itemtype="http://schema.org/Product">
     <div class="main col w730">
         <?php the_post_thumbnail(); ?>
