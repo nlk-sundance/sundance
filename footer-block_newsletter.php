ad593f1a4e506f5ca01ca6756f7491964a8070da
Tim Spinks <tim@ninthlink.com>
2016-02-25 12:20:39 -0800

    fixed newsletter form layout

--- footer-block_newsletter.php
+++ footer-block_newsletter.php
@@ -33,22 +33,22 @@
             <form action="http://login.sendmetric.com/phase2/bullseye/contactupdate1.php3" method="post" name="bullseye" id="bullseye" onsubmit="return doSubmit();" target="signup">
                 <table> 
                     <tr>
-                    <td><label for="firstname">Name</label></td>
-                    <td><input type="text" class="text" id="firstname" name="firstname" /></td>
+                    	<td><label for="firstname">Name</label></td>
+                    	<td><input type="text" class="text" id="firstname" name="firstname" /></td>
                     </tr>
                     <tr>
-                    <td><input type="hidden" name="cid" value="9bffaece0a39911668c51d8eb7af85c7" /><label for="email">Email</label></td>
-                    <td><input type="text" class="text" id="email" name="email" /></td>
+                    	<td><input type="hidden" name="cid" value="9bffaece0a39911668c51d8eb7af85c7" /><label for="email">Email</label></td>
+                    	<td><input type="text" class="text" id="email" name="email" /></td>
                     </tr>
                     <tr>
-                    <td><label for="postal_code">Zip</label></td>
-                    <td><input type="text" class="text zip" id="postal_code" name="postal_code" />
-                    </td>
+                    	<td><label for="postal_code">Zip</label></td>
+                    	<td><input type="text" class="text zip" id="postal_code" name="postal_code" /></td>
                     </tr>
-                    <td><input type="hidden" name="message" value="Thank you. Your information has been submitted. To ensure delivery of your newsletter(s), please add donotreply@sundancespas.com to your address book, spam filter whitelist, or tell your company's IT group to allow this address to pass through any filtering software they may have set up." />&nbsp;</td>
-                    <td><br /><input type="image" src="<?php bloginfo('template_url'); ?>/images/icons/dialogSubmit.png" value="submit" name="SubmitBullsEye" />
-                    <input type="hidden" name="grp[]" value="575636" /> 
-                    </td>
+                    <tr>
+                    	<td><input type="hidden" name="message" value="Thank you. Your information has been submitted. To ensure delivery of your newsletter(s), please add donotreply@sundancespas.com to your address book, spam filter whitelist, or tell your company's IT group to allow this address to pass through any filtering software they may have set up." />&nbsp;</td>
+                    	<td>
+                    		<br /><input type="image" src="<?php bloginfo('template_url'); ?>/images/icons/dialogSubmit.png" value="submit" name="SubmitBullsEye" /><input type="hidden" name="grp[]" value="575636" /> 
+                    	</td>
                     </tr>
                 </table>
             </form>
